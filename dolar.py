5ca96e94c5c1ad528c64f9876e078a57c8cc2d2f
Vai na Web Cpx <vainawebcpx@gmail.com>
2018-09-05 16:33:38 -0500

    python3

---
+++ dolar.py
@@ -0,0 +1,19 @@
+# reais = float(input('Quanto você tem na carteira?: '))
+# usa = reais/ 4.12;
+# euro = reais/ 4.80;
+# libra = reais/ 5.32;
+# print ('com o valor que você tem na carteira, você pode comprar {:.2f} Dolares, {:.2f} Euros, {:.2f} Libras,'.format(usa,euro,libra))
+
+
+tabuada = int(input('digite um numero inteiro: '))
+
+print('{} x {} = {}'.format(tabuada,1, tabuada*1))
+print('{} x {} = {}'.format(tabuada,2, tabuada*2))
+print('{} x {} = {}'.format(tabuada,3, tabuada*3))
+print('{} x {} = {}'.format(tabuada,4, tabuada*4))
+print('{} x {} = {}'.format(tabuada,5, tabuada*5))
+print('{} x {} = {}'.format(tabuada,6, tabuada*6))
+print('{} x {} = {}'.format(tabuada,7, tabuada*7))
+print('{} x {} = {}'.format(tabuada,8, tabuada*8))
+print('{} x {} = {}'.format(tabuada,9, tabuada*9))
+print('{} x {} = {}'.format(tabuada,10, tabuada*10))
